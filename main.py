a172ed8cb85dec2830956c92b6f81d481bd53097
aandresmon29 <71345919+aandresmon29@users.noreply.github.com>
2021-03-22 15:50:00 -0500

    Version with commint to frontend

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from models.user_models import UserIn, UserOut
 from models.indicador_models import IndicadorIn,IndicadorOut
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from routers.indicador_router import router as router_indicador
+from routers.user_router import router as router_user
 
 api = FastAPI()
 
-@api.get("/user/info/{username}")
-async def get_name(username: str):
-    user_in_db = get_user(username)
-    if user_in_db == None:
-        raise HTTPException(status_code=404, detail="El usuario no existe")
-    user_out = UserOut(**user_in_db.dict())
-    return user_out
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080","https://indicadorb.herokuapp.com",
+]
 
-@api.get("/Indicadores/{indicador}")
-async def get_indicor(indicador: str):
-    Indicador_in_db = get_indicador(indicador)
-    if Indicador_in_db == None:
-        raise HTTPException(status_code=404, detail="El indicador no existe")
-    indicador_out = IndicadorOut(**Indicador_in_db.dict())
-    return indicador_out
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
+api.include_router(router_user)
+api.include_router(router_indicador)
