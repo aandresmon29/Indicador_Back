7b36bf6ef10b1301800577511c41413e03443914
aandresmon29 <71345919+aandresmon29@users.noreply.github.com>
2021-05-10 10:24:30 -0500

    Update 10052021

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from fastapi import FastAPI, applications
 from fastapi import HTTPException
+from fastapi.exceptions import FastAPIError
 from fastapi.middleware.cors import CORSMiddleware
 from routers.indicador_router import router as router_indicador
 from routers.user_router import router as router_user
 from routers.gerencia_router import router as router_gerencia
 
-app = FastAPI()
+api = FastAPI()
 
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
     "http://localhost", "http://localhost:8080","https://indivp.herokuapp.com",
 ]
 
-app.add_middleware(
+FastAPIError.add_middleware(
     CORSMiddleware, allow_origins=origins,
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
 
-app.include_router(router_user)
-app.include_router(router_indicador)
-app.include_router(router_gerencia)
+api.include_router(router_user)
+api.include_router(router_indicador)
+api.include_router(router_gerencia)
